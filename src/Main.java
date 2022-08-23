ff8818cce02827c3bdb0e3c478969048b0c9ed4e
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-23 20:37:09 +0530

    it is an udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SumOddRange.sumOdd(1,25);
+        SumOddRange.sumOdd(1,100);
+        SumOddRange.sumOdd(-1,100);
     }
 }
--- src/SumOddRange.java
+++ src/SumOddRange.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SumOddRange {
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number < 0)
+            return false;
+        else {
+            if (number % 2 == 0)
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start > 0 && end > 0 && start<=end) {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i))
+                    sum += i;
+            }
+            return sum;
+        } else
+            return -1;
+
+    }
 }
